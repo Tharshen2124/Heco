3052222dd84fa8404355dac7f0af16dc958beec0
Tharshen2124 <tharshensurianbalan@gmail.com>
2023-07-29 16:06:37 +0800

    chore: install chakra ui

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import { Box, Container, Text } from "@chakra-ui/react"
+
+export default function Home() {
+  return (
+    <>
+      <Box>
+        <Text>hello</Text>
+      </Box>
+      <Container></Container>
+    </>
+  )
+}
