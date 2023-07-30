8da847d4cec70e1b3d405c05a31cbd5a90b2fc04
Melo04 <kohhmelody@gmail.com>
2023-07-31 02:40:10 +0800

    fix-review-component

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -4,14 +4,14 @@ export const Review = () => {
   return (
     <Box bg="gray.100" borderRadius="10px" boxShadow="xl">
       <HStack px="5" py="5" gap="3">
-        <Avatar alignSelf="flex-start" size="md" boxSize='2em'/>
+        <Avatar alignSelf="flex-start" boxSize='1.7em'/>
         <VStack gap={2}>
           <HStack justifyContent="space-between" w="100%">
-            <Text fontSize="md" fontWeight="bold" alignItems={"left"}>
+            <Text fontSize="sm" fontWeight="bold" alignItems={"left"}>
               John Doe
             </Text>
             <Text
-              fontSize="sm"
+              fontSize="13px"
               fontWeight="semibold"
               color="gray.500"
               alignItems={"right"}
@@ -19,7 +19,7 @@ export const Review = () => {
               One hour ago
             </Text>
           </HStack>
-          <Text fontSize="sm">
+          <Text fontSize="13px">
             I think the hospital is lit and really good. siodfdosdfjodsjf od
             nfosf osdfo sd sdof ds iofodjfo lsdfl jod osdf iosdjf osjd
           </Text>
