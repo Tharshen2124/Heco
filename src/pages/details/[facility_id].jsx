8f0cba060bd569ede0ab056c238780280ef24133
wyhong3103 <wyhong3103@gmail.com>
2023-08-03 16:05:30 +0800

    refactor: reformat code

--- src/pages/details/[facility_id].jsx
+++ src/pages/details/[facility_id].jsx
@@ -31,7 +31,13 @@ import { useEffect, useState } from "react";
 import Back from "../../../public/back.svg";
 import Image from "next/image";
 
-export default function Details({ facility_id, facility, review, images, data }) {
+export default function Details({
+  facility_id,
+  facility,
+  review,
+  images,
+  data,
+}) {
   const router = useRouter();
   const [cost, setCost] = useState(0);
   const [distance, setDistance] = useState(0);
@@ -43,7 +49,9 @@ export default function Details({ facility_id, facility, review, images, data })
 
   function openGoogleMap() {
     // Create the URL with the destination
-    const url = "https://www.google.com/maps/search/?api=1&query=" + encodeURIComponent(facility.address);
+    const url =
+      "https://www.google.com/maps/search/?api=1&query=" +
+      encodeURIComponent(facility.address);
     // Open the URL in a new tab/window
     window.open(url);
   }
@@ -98,143 +106,146 @@ export default function Details({ facility_id, facility, review, images, data })
       });
     });
     return () => {
-        navigator.geolocation.clearWatch(id);
-    }
+      navigator.geolocation.clearWatch(id);
+    };
   }, []);
 
   useEffect(() => {
     (async () => {
-        const sources = [`${coordinate.longitude},${coordinate.latitude}`, `${facility.longitude},${facility.latitude}`].join(';');
-        const url = `https://api.mapbox.com/directions-matrix/v1/mapbox/driving/${sources}?sources=0&annotations=distance&access_token=${process.env.NEXT_PUBLIC_MAP_BOX_ACCESS_TOKEN}`;
-        const response = await fetch(url);
-        const res_data = await response.json();
-        setDistance(res_data.distances[0][1] / 1000);
-    })()
+      const sources = [
+        `${coordinate.longitude},${coordinate.latitude}`,
+        `${facility.longitude},${facility.latitude}`,
+      ].join(";");
+      const url = `https://api.mapbox.com/directions-matrix/v1/mapbox/driving/${sources}?sources=0&annotations=distance&access_token=${process.env.NEXT_PUBLIC_MAP_BOX_ACCESS_TOKEN}`;
+      const response = await fetch(url);
+      const res_data = await response.json();
+      setDistance(res_data.distances[0][1] / 1000);
+    })();
   }, [coordinate]);
 
   return (
     <>
-    <Container px={[5, 0]} py={5} maxW={["100vw", "90vw"]} w={["100vw"]}>
+      <Container px={[5, 0]} py={5} maxW={["100vw", "90vw"]} w={["100vw"]}>
         <Box
-        _hover={{
+          _hover={{
             cursor: "pointer",
-        }}
-        w='40px'
+          }}
+          w="40px"
         >
-        <Image
+          <Image
             src={Back}
             alt="back"
             width="30px"
             height="30px"
             onClick={() => router.push("/")}
-        />
+          />
         </Box>
         <Flex direction="column" alignItems="center">
-        <Heading size="md" pt={5}>
+          <Heading size="md" pt={5}>
             {facility.name}
-        </Heading>
+          </Heading>
         </Flex>
-        <VStack >
-        <HStack pt={5}>
-            <Swiper
-            slidesPerView="auto"
-            freeMode={true}
-            modules={[FreeMode]}
-            spaceBetween={20}
-            >
-            {images.map((i) => {
-                return (
-                <SwiperSlide style={{ width: "auto" }} key={v4()}>
-                    <ChakraImage
-                    src={i}
-                    width={300}
-                    height={220}
-                    style={{ borderRadius: "20px" }}
-                    alt=""
-                    />
-                </SwiperSlide>
-                );
-            })}
-            </Swiper>
-        </HStack>
         <VStack>
+          <HStack pt={5}>
+            <Swiper
+              slidesPerView="auto"
+              freeMode={true}
+              modules={[FreeMode]}
+              spaceBetween={20}
+            >
+              {images.map((i) => {
+                return (
+                  <SwiperSlide style={{ width: "auto" }} key={v4()}>
+                    <ChakraImage
+                      src={i}
+                      width={300}
+                      height={220}
+                      style={{ borderRadius: "20px" }}
+                      alt=""
+                    />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </HStack>
+          <VStack>
             <HStack pt={5}>
-                <Text fontWeight="bold">Specialisation </Text>
-                <SearchIcon />
+              <Text fontWeight="bold">Specialisation </Text>
+              <SearchIcon />
             </HStack>
             <HStack mt={2}>
-                <Swiper
+              <Swiper
                 slidesPerView="auto"
                 freeMode={true}
                 modules={[FreeMode]}
                 spaceBetween={10}
-                >
+              >
                 {facility.tags.map((i, index) => (
-                    <SwiperSlide style={{ width: "auto" }} key={v4()}>
+                  <SwiperSlide style={{ width: "auto" }} key={v4()}>
                     <Tag
-                        px="20px"
-                        py="10px"
-                        borderRadius="10px"
-                        userSelect="none"
-                        fontSize="md"
-                        transition={"all 0.2s"}
-                        _hover={{
+                      px="20px"
+                      py="10px"
+                      borderRadius="10px"
+                      userSelect="none"
+                      fontSize="md"
+                      transition={"all 0.2s"}
+                      _hover={{
                         cursor: "pointer",
                         backgroundColor: "blue",
                         color: "white",
-                        }}
-                        marginLeft={index === 0 ? "10px" : 0}
-                        marginRight={
+                      }}
+                      marginLeft={index === 0 ? "10px" : 0}
+                      marginRight={
                         index === facility.tags.length - 1 ? "10px" : 0
-                        }
+                      }
                     >
-                        {i}
+                      {i}
                     </Tag>
-                    </SwiperSlide>
+                  </SwiperSlide>
                 ))}
-                </Swiper>
+              </Swiper>
             </HStack>
-            </VStack>
+          </VStack>
         </VStack>
 
         <Flex
-        gap={{ base: "10px", lg: "30px" }}
-        direction={{ base: "column", lg: "row" }}
-        mb="20"
-        justifyContent={"center"}
-        alignItems={{ base: "center", lg: "start" }}
-        w="100%"
+          gap={{ base: "10px", lg: "30px" }}
+          direction={{ base: "column", lg: "row" }}
+          mb="20"
+          justifyContent={"center"}
+          alignItems={{ base: "center", lg: "start" }}
+          w="100%"
         >
-        <Flex
+          <Flex
             gap="10px"
             direction="column"
             maxW="container.md"
             w={{ base: "100%", lg: "50%" }}
-        >
+          >
             <HStack pt={5}>
-            <Text fontWeight="bold">Location </Text>
-            <LocationOnIcon />
+              <Text fontWeight="bold">Location </Text>
+              <LocationOnIcon />
             </HStack>
             <Box
-            bg="gray.200"
-            borderRadius="10px"
-            boxShadow="xl"
-            mt={2}
-            px={5}
-            py={3}
+              bg="gray.200"
+              borderRadius="10px"
+              boxShadow="xl"
+              mt={2}
+              px={5}
+              py={3}
             >
-            <Text
+              <Text
                 fontSize="sm"
                 fontWeight="semibold"
                 color="black"
                 alignItems={"right"}
-            >
+              >
                 {facility.address}
-            </Text>
+              </Text>
             </Box>
 
             {facility.is24hour ? (
-            <Tag
+              <Tag
                 fontWeight="bold"
                 py={2}
                 mt={5}
@@ -242,139 +253,131 @@ export default function Details({ facility_id, facility, review, images, data })
                 bg="blue"
                 color="white"
                 maxW="165px"
-            >
+              >
                 <HStack alignItems="flex-start">
-                <AccessTimeIcon
+                  <AccessTimeIcon
                     style={{
-                    color: "white",
-                    fontSize: "22px",
+                      color: "white",
+                      fontSize: "22px",
                     }}
-                />
-                <Text>Open 24 hours</Text>
+                  />
+                  <Text>Open 24 hours</Text>
                 </HStack>
-            </Tag>
+              </Tag>
             ) : (
-            <>
+              <>
                 <HStack justifyContent="space-between" w="100%" pt={5}>
-                <Text w={40} fontWeight="bold">
+                  <Text w={40} fontWeight="bold">
                     Opening Time
-                </Text>
-                <HStack alignItems="center" gap={1}>
+                  </Text>
+                  <HStack alignItems="center" gap={1}>
                     <AccessTimeIcon style={{ color: "blue" }} />
                     <Text color="blue" fontWeight="bold">
-                    {facility.open_hour
-                        ? facility.open_hour + " AM"
-                        : "N/A"}
+                      {facility.open_hour ? facility.open_hour + " AM" : "N/A"}
                     </Text>
-                </HStack>
+                  </HStack>
                 </HStack>
                 <HStack justifyContent="space-between" w="100%" pt={5}>
-                <Text w={40} fontWeight="bold">
+                  <Text w={40} fontWeight="bold">
                     Closing Time
-                </Text>
-                <HStack alignItems="center" gap={1}>
+                  </Text>
+                  <HStack alignItems="center" gap={1}>
                     <AccessTimeIcon style={{ color: "blue" }} />
                     <Text color="blue" fontWeight="bold">
-                    {facility.close_hour
+                      {facility.close_hour
                         ? facility.close_hour + " PM"
                         : "Open 24 hours"}
                     </Text>
+                  </HStack>
                 </HStack>
-                </HStack>
-            </>
+              </>
             )}
             <Box
-            bg="gray.200"
-            borderRadius="10px"
-            boxShadow="xl"
-            mt={3}
-            px={5}
-            py={4}
+              bg="gray.200"
+              borderRadius="10px"
+              boxShadow="xl"
+              mt={3}
+              px={5}
+              py={4}
             >
-            <Text fontSize="md" fontWeight="semibold" color="black">
+              <Text fontSize="md" fontWeight="semibold" color="black">
                 Distance: {distance.toFixed(2)} km
-            </Text>
-            <HStack w="100%" justifyContent="space-between">
-                <Text
-                mt={2}
-                fontSize="md"
-                fontWeight="semibold"
-                color="black"
-                >
-                Cost Rating:
+              </Text>
+              <HStack w="100%" justifyContent="space-between">
+                <Text mt={2} fontSize="md" fontWeight="semibold" color="black">
+                  Cost Rating:
                 </Text>
                 <Progress
-                colorScheme={
+                  colorScheme={
                     cost >= 4 ? "green" : cost >= 3 ? "yellow" : "red"
-                }
-                width="170px"
-                mt={3}
-                borderRadius={20}
-                value={(cost / 5) * 100}
+                  }
+                  width="170px"
+                  mt={3}
+                  borderRadius={20}
+                  value={(cost / 5) * 100}
                 />
-            </HStack>
+              </HStack>
             </Box>
             <Button
-            bg="gray.800"
-            mt={8}
-            w="full"
-            color="white"
-            _hover={{
+              bg="gray.800"
+              mt={8}
+              w="full"
+              color="white"
+              _hover={{
                 background: "blue",
-            }}
-            onClick={openGoogleMap}
+              }}
+              onClick={openGoogleMap}
             >
-            <LocationOnIcon /> Direction
+              <LocationOnIcon /> Direction
             </Button>
-            
-        </Flex>
+          </Flex>
 
-        <Flex direction="column" gap="10px" w="100%" maxW="container.md">
+          <Flex direction="column" gap="10px" w="100%" maxW="container.md">
             <HStack justifyContent="space-between" w="100%" pt={5}>
-            <Text fontWeight="bold">Reviews</Text>
-            <HStack
+              <Text fontWeight="bold">Reviews</Text>
+              <HStack
                 alignItems="center"
                 color={
-                sentiment === "Mostly Positive"
+                  sentiment === "Mostly Positive"
                     ? "#2DFF00"
                     : sentiment === "Neutral"
                     ? "black"
                     : "red"
                 }
                 gap={1}
-            >
+              >
                 <TagFacesIcon />
                 <Text fontWeight="bold">{sentiment}</Text>
-            </HStack>
+              </HStack>
             </HStack>
             <Divider mt={2} bg="gray.800" borderWidth="1px" />
             <VStack mt={3} px={-5} gap={5}>
-            {review.map((i) => {
+              {review.map((i) => {
                 return <Review review={i} key={v4()} />;
-            })}
-                  <Button
-                    w="100%"
-                    maxW="container.md"
-                    bg="blue"
-                    color={"white"}
-                    _hover={{
-                      backgroundColor: "#d6d8ff",
-                      color: "#020ad4",
-                    }}
-                    _active={{
-                      backgroundColor: "#020ad4",
-                    }}
-                    onClick={openReview}
-                  >
-                    <Center>
-                      <EditIcon mr={2} />
-                      <Text>Create Review</Text>
-                    </Center>
-                  </Button>
+              })}
+              <Button
+                w="100%"
+                maxW="container.md"
+                bg="blue"
+                color={"white"}
+                _hover={{
+                cursor: "pointer",
+                backgroundColor: "#020ad4",
+                }}
+                _active={{
+                backgroundColor: "#020ad4",
+                }}
+                onClick={openReview}
+              >
+                <Center>
+                  <EditIcon mr={2} />
+                  <Text>Create Review</Text>
+                </Center>
+              </Button>
             </VStack>
+          </Flex>
         </Flex>
-        </Flex>
-    </Container>
+      </Container>
     </>
   );
 }
@@ -386,7 +389,7 @@ export async function getServerSideProps(context) {
   const images = await apiHandler.getFacilityImage(params.facility_id);
   return {
     props: {
-      facility_id : params.facility_id,
+      facility_id: params.facility_id,
       facility,
       review,
       images,
